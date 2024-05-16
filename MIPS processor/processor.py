2b45f19cd6066cdd672c564a745a2eda5a4788ac
Krish Patel <144411330+kodercrish@users.noreply.github.com>
2024-05-16 10:01:24 +0530

    Update processor.py

--- MIPS processor/processor.py
+++ MIPS processor/processor.py
@@ -1,4 +1,4 @@
-with open('binary_3.txt', 'r') as file1, open('byte_add_mem.txt', 'w') as file2:
+with open('binary_3.txt', 'r') as file1, open('byte_add_mem.txt', 'w') as file2:  //While running this code, change file1 here
   ins = file1.read().splitlines()
   for i in ins:
     line1 = i[0:8]
